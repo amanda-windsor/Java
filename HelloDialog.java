5d91d3fc854eb912e92fe08cf912c99b43c3f682
Amanda <windsoramanda@outlook.com>
2022-11-05 19:39:39 +1000

    Create HelloDialog.java

---
+++ HelloDialog.java
@@ -0,0 +1,12 @@
+// Filename HelloDialog.java
+// Written by Amanda Windsor
+// Written on 05/11/2022
+
+import javax.swing.JOptionPane; // This built-in class produces GUI output.
+public class HelloDialog
+{
+  public static void main(String[] args)
+  {
+    JOptionPane.showMessageDialog(null, "Hello, world!");
+   }
+ }
